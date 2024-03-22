3f982e5da2fc8985d8335df4524c6d0a63eab336
일혜쓰 <ilhyesseu@appleui-iMac-4.skbroadband>
2024-03-22 10:29:35 +0900

    finish project

--- js/custom.js
+++ js/custom.js
@@ -154,7 +154,7 @@ document.addEventListener('DOMContentLoaded', function () {
   function benefitsAnimation() {
     const benefits_nums = gsap.utils.toArray('.benefits_num');
 
-    benefits_nums.forEach((num) => {
+    benefits_nums.forEach((num, i) => {
       const data_speed = num.getAttribute('data-speed');
 
       tl.from(num, {
@@ -187,19 +187,24 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   workAnimation();
-  function serviceAnimation() {
+
+  const pcDataSpeed = ['500', '400', '800', '600'];
+  const mobileDataSpeed = ['150', '180', '250', '180'];
+
+  function serviceAnimation(speed) {
     const arrow_elmts = gsap.utils.toArray('.service-arrow');
 
-    arrow_elmts.forEach((num) => {
-      const data_speed = num.getAttribute('data-speed');
+    arrow_elmts.forEach((num, i) => {
+      console.log(speed[i]);
+      // const data_speed = num.getAttribute('data-speed');
 
       tl.from(num, {
         scrollTrigger: commonScrollTrigger.service,
-        x: -data_speed,
+        x: -speed[i],
       });
     });
   }
-  serviceAnimation();
+
   function footerAnimation() {
     const letter_elmts = gsap.utils.toArray('.footer-wrapper span');
 
@@ -219,8 +224,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
   if (wWidth > 1300) {
     headerAnimation(-70);
+    serviceAnimation(pcDataSpeed);
   } else {
     headerAnimation(0);
+    serviceAnimation(mobileDataSpeed);
   }
 
   //================================
